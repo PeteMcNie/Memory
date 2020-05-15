4f3d809ce22b1d2d12ca33dd2c5dbe5e70f638f1
petemcnie <petemcnie0@gmail.com>
2020-05-15 14:25:07 +1200

    Game options complete

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let card = document.querySelectorAll('.cards');
 let cardFlipped = false;
+let lockCards = false;
 let firstCard;
 let secondCard;
 
+
 function flip () {
+    if(lockCards) return;
+    if(this === firstCard) return;
     this.classList.toggle('flip')
 
     if(!cardFlipped) {
@@ -12,7 +16,6 @@ function flip () {
         return;
     }
     secondCard = this;
-    cardFlipped = false;
 
     checkForMatch();
 }
@@ -23,7 +26,6 @@ function checkForMatch () {
         disableCards();
         return;
     }
-
     unflipCards();
 }
 
@@ -32,15 +34,37 @@ function disableCards () {
     firstCard.removeEventListener('click', flip);
     secondCard.removeEventListener('click', flip);
 
+    resetCards();
 }
 
 
 function unflipCards () {
+    lockCards = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetCards();
     }, 1500)
 }
+
+
+function resetCards () {
+    cardFlipped = false;
+    lockCards = false;
+    firstCard = null;
+    secondCard = null;
+}
+
+
+(function shuffle() {
+    card.forEach(card => {
+        let randomPlace = Math.floor(Math.random() * 12);
+        card.style.order = randomPlace;
+    })
+})();
+
+
 
 card.forEach(image => image.addEventListener('click', flip));
 
