85e9a18150cb972a8c71b40d9160487ca343f905
petemcnie <petemcnie0@gmail.com>
2020-05-15 13:28:54 +1200

    Images linked to html

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-let images = document.querySelectorAll('.cards');
+
+
+let card = document.querySelectorAll('.cards');
 
 function flip () {
     this.classList.toggle('flip')
 }
-
-images.forEach(image => image.addEventListener('click', flip));
-
+card.forEach(image => image.addEventListener('click', flip));
 
 
 
+
+
+
+
+
+
+
+// function shuffle () {
+//   for (let i = 0; i < card.length; i++) {
+
+// }
+
+// console.log(shuffle())
