4aed350dcd7cd44a61a56eed7c6364e033125a6b
petemcnie <petemcnie0@gmail.com>
2020-05-15 13:54:14 +1200

    background style changed

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-
-
 let card = document.querySelectorAll('.cards');
+let cardFlipped = false;
+let firstCard;
+let secondCard;
 
 function flip () {
     this.classList.toggle('flip')
+
+    if(!cardFlipped) {
+        cardFlipped = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    cardFlipped = false;
+
+    checkForMatch();
 }
+
+
+function checkForMatch () {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+
+function disableCards () {
+    firstCard.removeEventListener('click', flip);
+    secondCard.removeEventListener('click', flip);
+
+}
+
+
+function unflipCards () {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500)
+}
+
 card.forEach(image => image.addEventListener('click', flip));
 
 
 
 
-
-
-
-
-
-
-// function shuffle () {
-//   for (let i = 0; i < card.length; i++) {
-
-// }
-
-// console.log(shuffle())
